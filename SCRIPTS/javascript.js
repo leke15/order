0d50071d3be46d9b99cb4264f8db3d3fb1dcc09b
Daniel Ayomidotun <58265059+leke15@users.noreply.github.com>
2023-12-30 12:31:55 +0200

    comments

--- SCRIPTS/javascript.js
+++ SCRIPTS/javascript.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  
+// the add button hover animation
     $("#add").hover(hoverbtn,function(){
         $(this).css({
         "box-shadow":"",
@@ -7,14 +7,15 @@ $(document).ready(function(){
         "transition":"all 0.2s ease-in-out"
     });
     });
-    
+
+
+ // the function responsible for the add button animation   
     function hoverbtn (){
         $(this).css({
         "box-shadow":"0 5px 0px 0px rgb(255, 167, 66)",
         "transform":"scale(1.1)",
         "transition":"all 0.2s ease-in-out"
     });
-
     }
 
 });
