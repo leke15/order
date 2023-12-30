8a6bf9657f421d79cfac63ad4466e095b4265eed
Daniel Ayomidotun <58265059+leke15@users.noreply.github.com>
2023-12-30 14:33:24 +0200

    added the section for new notes and styled the website as well as the save btn

---
+++ SCRIPTS/newnote.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("#savebtn").hover(hoverbtn,function(){
+        $(this).css({
+        "box-shadow":"",
+        "transform":"scale(1)",
+        "transition":"all 0.2s ease-in-out"
+    });
+    });
+    
+    function hoverbtn (){
+        $(this).css({
+        "box-shadow":"0 5px 0px 0px rgb(255, 167, 66)",
+        "transform":"scale(1.1)",
+        "transition":"all 0.2s ease-in-out"
+    });
+    }
+});
